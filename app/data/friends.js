8d7007c694ec215e9d0e4629c44d5307c56bf5c1
Cohen <dcoco91@gmail.com>
2019-08-15 13:12:39 -0400

    added the necessary node.js modules to run the application. started working on the survey page/form, and added some data into the friends file

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [{
+        "name": "Daniel",
+        "photo": "https://dcoco1890.github.io/Cohen_portfolio/assets/images/me_is.png",
+        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name": "Herman",
+        "photo": "https://i.imgur.com/7tthR.jpg",
+        "scores": [1, 4, 2, 3, 1, 2, 5, 5, 5, 5]
+    },
+    {
+        "name": "Steve",
+        "photo": "https://i.imgur.com/PRrbqkh.jpg",
+        "scores": [5, 5, 2, 3, 4, 1, 1, 3, 5, 2]
+
+    }
+];
+
+module.exports = friends
