af6e58316df906e54de5a532a874244d2091c0cd
Cohen <dcoco91@gmail.com>
2019-08-16 11:16:26 -0400

    added an html route to redirect users to the Github page of the project. Also added a way for the server to serve up static images because I wanted a pretty pattern background

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,6 +8,10 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
+    // when the Github Repo page is clicked, it redirects to my Gihub page for the project
+    app.get("/git", function(req, res) {
+        res.redirect("https://github.com/dcoco1890/FindAFriend");
+    })
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+//not sure what this does but it seems important
 app.use(express.urlencoded({ extended: true }));
+//to serve JSON data
 app.use(express.json());
+//to serve static files (background Image) with express
+app.use(express.static("./app/public"));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
