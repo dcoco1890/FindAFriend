368cfafd091cbe35391b9aeca2a690f97350dd2d
Cohen <dcoco91@gmail.com>
2019-08-16 17:47:58 -0400

    created a click method for the form, working on validating all the data being sent the express server

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,8 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(req, res) {
-        friends.push(req.body);
-        res.json(true);
+        console.log(req.body);
+        // friends.push(req.body);
+        res.json(req.body);
     })
 }
