a936c47a8b797839f4c67d2675c73c221b1f016d
Cohen <dcoco91@gmail.com>
2019-08-17 16:32:39 -0400

    fixed an issue where form data was being sent to the wrong place

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,13 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
         console.log(req.body);
-        friends.push(req.body);
+        // friends.push(req.body);
         // res.json(req.body);
+        var newName = req.body.name;
+        var newImg = req.body.photo;
+        var newScores = req.body.scores.map(x => Number(x));
+        console.log(newScores);
+        console.log(newImg, newName);
+        res.send(true);
     })
 }
